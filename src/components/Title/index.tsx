413a32e55e6dd139e033e93767f903b789f0c417
Thiago Henrique Fragata <thiago.henrique.fragata@hotmail.com>
2021-11-12 04:40:14 -0400

    TB-fix: animação nos titulos da pagina

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 import { ContainerDefault, TitleContent } from "../../styles/commonStyles";
 
 interface TitleProps {
@@ -8,9 +9,11 @@ interface TitleProps {
 export function Title({ title }: TitleProps) {
   return (
     <ContainerDefault>
-      <TitleContent>
-        <h1>{title}</h1>
-      </TitleContent>
+      <Fade direction="down" delay={100}>
+        <TitleContent>
+          <h1>{title}</h1>
+        </TitleContent>
+      </Fade>
     </ContainerDefault>
   );
 }
